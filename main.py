73a3a113716397e6a61bd65386c313e1312ca3e7
Rafay Qureshi <mrafayyqureshi@gmail.com>
2023-07-17 17:00:18 +0500

    Switch Values

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 # Python print code
-print("Hello World: ")
+# print("Hello World: ")
 
 # Python Variable
 
-name = input("What is Your Name: ")
-length = len(name)
-print(length)
+# name = input("What is Your Name: ")
+# length = len(name)
+# print(length)
+
+#Switch Values
+
+a = input("a= ")
+b = input("b= ")
+
+c = a
+a = b
+b = c
+
+print("a= ", a)
+print("b= ", b)
