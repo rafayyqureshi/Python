56e43b26cd0add54694e6b7b21f41f06022a65bf
Rafay Qureshi <mrafayyqureshi@gmail.com>
2023-07-17 21:24:02 +0500

    Day1 Completed

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ print("b= ", b)
 # Band Name Generator
 
 name = input("What is your name? \n")
+
 city = input("what is your city name? \n ")
 
 print("Your band name could be " + name + " " + city)
