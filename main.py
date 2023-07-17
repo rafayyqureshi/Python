7e128af10e17c11e98c863bbd1c81f528611008c
Rafay Qureshi <mrafayyqureshi@gmail.com>
2023-07-17 18:03:33 +0500

    Band name generator

--- main.py
+++ main.py
@@ -9,12 +9,19 @@
 
 #Switch Values
 
-a = input("a= ")
-b = input("b= ")
+# a = input("a= ")
+# b = input("b= ")
 
-c = a
-a = b
-b = c
+# c = a
+# a = b
+# b = c
 
-print("a= ", a)
-print("b= ", b)
+# print("a= ", a)
+# print("b= ", b)
+
+# Band Name Generator
+
+name = input("What is your name? \n")
+city = input("what is your city name? \n ")
+
+print("Your band name could be " + name + " " + city)
