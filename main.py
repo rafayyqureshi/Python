6d34f1c67599335a99c1f8ce857ef17921102883
Rafay Qureshi <mrafayyqureshi@gmail.com>
2023-07-21 15:34:56 +0500

    Leap Year

--- main.py
+++ main.py
@@ -170,16 +170,24 @@
 
 
 # BMI CALCULATOR 2.0
-weight = input("Please enter weight: ")
-height = input("Please enter height: ")
-bmi = round(int(weight) / float(height) ** 2)
-if bmi <= 18.5:
-    print(f"Your BMI id {bmi}. You are under weight.")
-elif bmi <= 25 :
-    print(f"Your BMI id {bmi}. You have a normal weight.")
-elif bmi <= 30:
-    print(f"Your BMI id {bmi}. You are over weight.") 
-elif bmi <= 35:
-    print(f"Your BMI id {bmi}. You are over obese.") 
+# weight = input("Please enter weight: ")
+# height = input("Please enter height: ")
+# bmi = round(int(weight) / float(height) ** 2)
+# if bmi <= 18.5:
+#     print(f"Your BMI id {bmi}. You are under weight.")
+# elif bmi <= 25 :
+#     print(f"Your BMI id {bmi}. You have a normal weight.")
+# elif bmi <= 30:
+#     print(f"Your BMI id {bmi}. You are over weight.") 
+# elif bmi <= 35:
+#     print(f"Your BMI id {bmi}. You are over obese.") 
+# else:
+#     print("You are above 35. You are clinically abese.")
+
+
+# Leap Year
+year = int(input("Enter Year: "))
+if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+    print("It's a leap year.")
 else:
-    print("You are above 35. You are clinically abese.")
+    print("Not a leap year.")
