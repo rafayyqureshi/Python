87554ffde1ea92c38d2fc121606da37bda3ae857
Rafay Qureshi <mrafayyqureshi@gmail.com>
2023-07-22 10:56:36 +0500

    multiple if

--- main.py
+++ main.py
@@ -145,20 +145,33 @@
 
 #Conditional Operator / if / else
 
-# print("Welcome to the rollercoaster!")
-# height = int(input("Please enter your height: "))
-# if height >= 120:
-#     print("You can ride the rollercoaster.")
-#     age = int(input("Enter your age: "))
-#     if age < 12:
-#         print("Please pay $5")
-#     elif age <18:
-#         print("Please pay $7")
-#     else:
-#         print("Please pay $12")
-# else:
-#     print("Sorry, you need to grow taller before you can ride.")
+print("Welcome to the rollercoaster!")
+height = int(input("Please enter your height in cm: "))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster.")
+    age = int(input("Enter your age: "))
+    if age < 12:
+        bill = 5
+        print("Please pay $5")
+    elif age <18:
+        bill = 7
+        print("Please pay $7")
+    else:
+        bill = 12
+        print("Please pay $12")
 
+
+    photo = input("Do you want a photo taken? Y or N: ")
+    if photo == "Y":
+        bill += 3
+
+        print(f"Your final bill is {bill}.")
+
+
+else:
+    print("Sorry, you need to grow taller before you can ride.")
+    
 #Modulo
 
 # num = input("Enter any number: ")
@@ -185,9 +198,9 @@
 #     print("You are above 35. You are clinically abese.")
 
 
-# Leap Year
-year = int(input("Enter Year: "))
-if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
-    print("It's a leap year.")
-else:
-    print("Not a leap year.")
+# # Leap Year
+# year = int(input("Enter Year: "))
+# if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+#     print("It's a leap year.")
+# else:
+#     print("Not a leap year.")
